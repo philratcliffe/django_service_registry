26e8f9476dd0dae3b903615f81ab53e65a992477
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-01-28 16:35:31 +0000

    Finished off endpoint routing.

--- services/urls.py
+++ services/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.service_list, name='service_list'),
-    path('<service>', views.service_list, name='service_list'),
+    path('v1', views.list_or_add, name='list_or_add_endpoint'),
+    path('v1/<int:pk>', views.update, name='update_endpoint'),
+    path('v1/<service>', views.find_or_delete, name='find_or_delete_endpoint'),
+    path(
+        'v1/<service>/<version>',
+        views.find_or_delete,
+        name='find_or_delete_endpoint'),
 ]
